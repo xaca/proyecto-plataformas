bcfd06f0dd2fd45e894eab3714458013351158b7
Andres Bedoya Tobon <andres.bedoya@upb.edu.co>
2022-04-20 19:17:56 -0500

    Integracion de login, pendiente prueba localstorage

--- js/registro.js
+++ js/registro.js
@@ -1,14 +1,16 @@
 let btn_enviar, btn_cerrar_modal, formulario, ventana;
-let correo,clave, usuario;
+let correo,clave,nombre,celular, usuario;
 
 window.onload = function(){
 	btn_enviar = document.getElementById("btn_enviar");
 	btn_cerrar_modal = document.getElementById("btn_cerrar_modal");
 	correo = document.getElementById("correo");
+	nombre = document.getElementById("nombre");
+	celular = document.getElementById("celular");
 	clave = document.getElementById("clave");
 	ventana = document.getElementById("ventana");
 	formulario = document.getElementById("formulario");
-	formulario.addEventListener("submit",procesarEnvio);
+	formulario.addEventListener("submit",procesarRegistro);
 	btn_cerrar_modal.addEventListener("click",cerrarVentana);
 
 	setTimeout(hideURLbar, 0);
@@ -23,16 +25,20 @@ function cerrarVentana(evento){
 	location.href = "login.html";
 }
 
-function procesarEnvio(evento){
+function procesarRegistro(evento){
 
-	let txt_correo,txt_clave;
+	let txt_correo,txt_clave,txt_celular,txt_nombre;
 	let str_usuario;
 	txt_correo = correo.value;
+	txt_nombre = nombre.value;
+	txt_celular = celular.value;
 	txt_clave = md5(clave.value);
 
 	usuario = {
 		correo:txt_correo,
-		clave:txt_clave
+		clave:txt_clave,
+		celular:txt_celular,
+		nombre:txt_nombre
 	};
 
 	str_usuario = JSON.stringify(usuario);
